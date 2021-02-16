9c198a43931ad99e4bcc94b92d39e1da4144ff0e
Preeti12345678 <75837980+Preeti12345678@users.noreply.github.com>
2021-02-16 19:51:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ function getParagraph1(){
     for(var i=1; i<6; i++){
         inputs.push(document.getElementById("para_input_box_" +i).value);
         inputs.join(".");
-        document.getElementById("ShowParagraph").innerHTML=inputs.join(".");
+        document.getElementById("ShowParagraph1").innerHTML=inputs.join(".");
     }
 
 }
 function getParagraph2(){
-    for(var i=7; i<12; i++){
+    for(var i=6; i<13; i++){
         inputs.push(document.getElementById("para_input_box_" +i).vlaue);
         inputs.join(".");
-        document.getElementById("ShowParagraph").innerHTML=inputs.join(".");
+        document.getElementById("ShowParagraph2").innerHTML=inputs.join(".");
     }
 }
