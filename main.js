85ef0f0d76e27440ea943ec30c7f114db8406e21
Preeti12345678 <75837980+Preeti12345678@users.noreply.github.com>
2021-02-19 11:42:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-var inputs=[];
+
 
 function getParagraph1(){
+    var inputs=[];
     for(var i=1; i<6; i++){
         inputs.push(document.getElementById("para_input_box_" +i).value);
         inputs.join(".");
@@ -9,6 +10,7 @@ function getParagraph1(){
 
 }
 function getParagraph2(){
+    var inputs=[];
     for(var i=6; i<13; i++){
         inputs.push(document.getElementById("para_input_box_" +i).vlaue);
         inputs.join(".");
